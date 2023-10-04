2c2efc75eb76739312fed6bfa9e353e9f137662e
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-10-04 00:20:43 +0000

    Insert a node in a sequence

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_node - insert a node in a sorted linked list
+ * @head: head of list
+ * @number: number to insert
+ * Return: Address of new node else NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t  *temp, *new_node, *prev;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	temp = *head;
+	prev = NULL;
+
+	new_node->n = number;
+	new_node->next = NULL;
+
+	if (temp == NULL || temp->n >= number)
+	{
+		new_node->next = temp;
+		temp = new_node;
+		return (new_node);
+	}
+
+	while (temp != NULL && temp->n < number)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+	new_node->next = temp;
+	prev->next = new_node;
+	return (new_node);
+}
