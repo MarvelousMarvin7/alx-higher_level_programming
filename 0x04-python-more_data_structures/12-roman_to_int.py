33526c4be4e729b930435c13405abba45181048c
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-10-14 01:34:56 +0000

    covert roman numerals to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,4 +3,32 @@
 
 
 def roman_to_int(roman_string):
-    
+    # check if roman_string is not a string or empty
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    # create a dictionary for their integer values
+    roman_dict = {
+        'I': 1
+        'V': 5
+        'X': 10
+        'L': 50
+        'C': 100
+        'D': 500
+        'M': 100
+    }
+    total = 0
+    prev_value = 0
+
+    for symbol in roman_string:
+        if symbol not in roman_dict:
+            return 0
+        value = roman_dict[symbol]
+
+        if value > prev_value:
+            total += value - 2 * prev_value
+        else:
+            total += value
+
+        prev_value = value
+
+    return total
