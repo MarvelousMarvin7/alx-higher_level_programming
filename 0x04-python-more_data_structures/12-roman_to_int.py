6bb854a3761e6e1ea6e8c688745cfd83dbda0e21
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-10-14 01:46:31 +0000

    roman to numerals edit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,12 +8,12 @@ def roman_to_int(roman_string):
         return 0
     # create a dictionary for their integer values
     roman_dict = {
-        'I': 1
-        'V': 5
-        'X': 10
-        'L': 50
-        'C': 100
-        'D': 500
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
         'M': 1000
     }
     total = 0
