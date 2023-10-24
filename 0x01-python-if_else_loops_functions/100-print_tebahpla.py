94e959157f553f5dfacb2ba72609648c25ab9baa
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-10-24 11:15:01 +0000

    print recersed alphabet

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Print ASCII characters in reverse order"""
+
+
+i = 0
+for char in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(char - i)), end="")
+    i = 32 if i == 0 else 0
