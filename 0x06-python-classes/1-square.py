c4b15f5480d8509b8f68dc4aa591e51291acec3b
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-10-25 01:24:10 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    """Define a square based on 0-square"""
+
+    def __init__(me, size):
+        """make size of square private"""
+        me.__size == size
