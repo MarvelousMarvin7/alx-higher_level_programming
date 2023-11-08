45fe0bf3ec1a1cd9a4a0d1835be181eca421cf1e
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-11-08 11:32:48 +0000

    Rectangle

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Real Definition of a rectangle"""
+
+
+class Rectangle:
+    """
+    Initialise with and height with value checks
+
+    Returns:
+    Width and Height
+
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """ width fo the triangle"""
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    """height of the triangle"""
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """Find area of a triangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Find perimeter of a triangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * self.__width) + (2 * self.__height)
+
+    def __str__(self):
+        """print rectangle with '#'"""
+        string = ""
+        if self.__width == 0 or self.__height == 0:
+            return string
+        for h in range(self.__height):
+            for w in range(self.__width):
+                string += "#"
+            if h is not self.__height - 1:
+                string += "\n"
+        return string
+
+    def __repr__(self):
+        """ return a string representation of the rectangle"""
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
