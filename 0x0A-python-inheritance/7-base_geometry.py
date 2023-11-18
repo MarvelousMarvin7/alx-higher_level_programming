add8a91cc648f0f6bec85d68bed0fc4cf4903e7e
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-11-18 23:26:34 +0000

    Geometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -24,7 +24,7 @@ class BaseGeometry:
         TypeError: <name> must be an integer
         ValueError: <name> must be greater than 0
         """
-        if type(value) !=  int:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
