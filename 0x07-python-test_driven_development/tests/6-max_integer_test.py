d8c8acebdb265dd7dd58508cc9bba632edb196b4
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-12-04 00:38:21 +0000

    max integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,34 +4,36 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """Test for max_integer function"""
 
     def setUp(self):
-            pass
+        pass
 
     def tearDown(self):
-            pass
+        pass
 
     def test_docstring(self):
-            self.assertIsNotNone(max_integer, __doc__)
+        self.assertIsNotNone(max_integer, __doc__)
 
     def test_normal_int(self):
-            self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-	    self.assertEqual(max_integer([1, 3, 4, 2]), 4)
-	    self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
-	    self.assertEqual(max_integer([4, 3, 2, 1]), 4)
-	    # only one element
-	    self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+	# only one element
+        self.assertEqual(max_integer([1]), 1)
 
     def test_neg_int(self):
-            self.assertEqual(max_integer([1, -2, 3, 4]), 4)
-	    self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([1, -2, 3, 4]), 4)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
-            with self.assertRaises(TypeError):
-		max_integer(None)
+        with self.assertRaises(TypeError):
+            max_integer(None)
 
-    if __name__ == "__main__":
-        unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
