746e52a8fc3abfb0070ebae2ae5d35628b04592b
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-02-22 19:39:28 +0000

    Issubclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Only sub class of"""
+
+
+def inherits_from(obj, a_class):
+    """ returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False.
+
+    Args:
+    obj : object to check
+    a_class : class to verify
+
+    Return:
+    True else False
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
