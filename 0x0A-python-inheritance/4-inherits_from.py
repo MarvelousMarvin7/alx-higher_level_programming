d997fa6ed9f5c92d07d3b2497a668e1f39385112
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-02-23 21:51:12 +0000

    Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,6 +13,6 @@ def inherits_from(obj, a_class):
     Return:
     True else False
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
