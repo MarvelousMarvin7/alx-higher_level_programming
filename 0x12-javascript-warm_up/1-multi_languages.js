80799bb85b93bc3bdfc08fa9e9a51c45e1b142bb
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-06-21 18:48:53 +0000

    Javascript introduction

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const Firstline = 'C is fun';
+const Secondline = 'Python is cool';
+const Thirdline = 'Javascript is Amazing';
+console.log(Firstline);
+console.log(Secondline);
+console.log(Thirdline);
