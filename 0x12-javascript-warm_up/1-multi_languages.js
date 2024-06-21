f3a302a0005b769e74dcb8ad777773379240dbba
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-06-21 23:13:27 +0000

    to number

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const Firstline = 'C is fun';
 const Secondline = 'Python is cool';
-const Thirdline = 'Javascript is Amazing';
+const Thirdline = 'Javascript is amazing';
 console.log(Firstline);
 console.log(Secondline);
 console.log(Thirdline);
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const argsOne = args[0];
+const argsTwo = args[1];
+
+console.log(`${argsOne} is ${argsTwo}`);
