cd5dbecd39dcef0aad0f652bae2271c9a84a9073
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-06-23 07:09:09 +0000

    Value fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-if (process.argv[2] === undefined) {
+if (args[0] === undefined) {
   console.log('No argument');
 } else {
-  args.forEach((arg) => {
-    console.log(`${arg}`);
-  });
+  console.log(args[0]);
 }
