4b9598d552485c575503425110ba1e1e6e9cd762
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-06-23 06:05:49 +0000

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,10 @@
 const args = process.argv.slice(2).map(Number);
 
 if (args.length <= 1) {
-    console.log(0);
+  console.log(0);
 } else {
-    const second = args.sort(function (a, b) {
-        return b - a;
-    })[1];
-    console.log(second);
+  const second = args.sort(function (a, b) {
+    return b - a;
+  })[1];
+  console.log(second);
 }
