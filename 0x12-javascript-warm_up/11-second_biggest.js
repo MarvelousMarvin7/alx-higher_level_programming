a8ac38575932d7434df963bb69aa5521ec80900f
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-06-23 05:52:04 +0000

    Javascript objects

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
+    console.log(0);
+} else {
+    const second = args.sort(function (a, b) {
+        return b - a;
+    })[1];
+    console.log(second);
+}
