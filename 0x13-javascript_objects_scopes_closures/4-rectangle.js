38f0f9727d81a68f64ed0fa1057f9f4063948b5d
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-06-27 21:12:13 +0000

    Js objects and scopes

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let size = '';
+      for (let j = 0; j < this.width; j++) {
+        size += 'X';
+      }
+      console.log(size);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+
+}
+
+module.exports = Rectangle;
