0dba551d8366db85a66910abbaf462887f90ff63
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-07-14 09:14:46 -0700

    Curl

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/pyhton3
+"""finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    if (list_of_integers) is None or list_of_integers == []:
+        return None
+
+    high = len(list_of_integers) - 1
+    low = 0
+
+    while low <= high:
+        mid = ((high - low) // 2) + low
+
+        if high == 0:
+            return list_of_integers[0]
+        if high == 1:
+            return max(list_of_integers)
+        if list_of_integers[mid] >= list_of_integers[mid - 1] and \
+           list_of_integers[mid] >= list_of_integers[mid + 1]:
+            return list_of_integers[mid]
+        elif mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid - 1
